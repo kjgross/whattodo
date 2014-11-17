de42888a6f48b03b77dcce779ae771ecaafc360a
Kelsey <kelsey@edmodo.com>
2014-11-17 07:54:33 -0800

    remove two extra files

--- cards/api.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from flask import request, Response, url_for
-from jsonschema import validate, ValidationError
-
-import models
-import decorators
-from cards import app
-from database import session
